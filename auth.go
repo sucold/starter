e934da15abad08d41964282c05ea614768d86d80
hinego <89022212+hinego@users.noreply.github.com>
2022-11-17 18:18:19 +0800

    Initial commit

---
+++ auth.go
@@ -0,0 +1,220 @@
+package conset
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"github.com/gogf/gf/v2/container/gvar"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/util/gconv"
+	"github.com/gogf/gf/v2/util/grand"
+	"github.com/hinego/authentic"
+	"github.com/hinego/errorx"
+	"github.com/hinego/starter/app/cache"
+	"github.com/hinego/starter/app/conset/api"
+	"github.com/hinego/starter/app/consts"
+	"github.com/hinego/starter/app/dao"
+	"github.com/hinego/starter/app/model"
+	"github.com/hinego/starter/app/service"
+	"github.com/hinego/starter/app/service/mail"
+	"github.com/samber/lo"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"strings"
+	"time"
+)
+
+type authController struct{}
+type authedController struct{}
+
+var (
+	Auth                 = authController{}
+	Authed               = authedController{}
+	ErrIncorrectUserName = errorx.New("账号或密码错误")
+)
+
+func (c *authController) Login(ctx context.Context, req *api.AuthLoginReq) (res *api.AuthLoginRes, err error) {
+	var (
+		user *model.User
+	)
+	if user, err = dao.User.Where(dao.User.Email.Like(req.Username)).First(); err != nil {
+		return nil, ErrIncorrectUserName
+	} else if err = bcrypt.CompareHashAndPassword([]byte(user.Password), consts.Salt(req.Password)); err != nil {
+		return nil, ErrIncorrectUserName
+	} else {
+		var token *authentic.Context
+		if token, err = service.Auth.CreateToken(ctx, map[string]any{consts.UserKey: user.ID, "role": user.Role}); err != nil {
+			return nil, err
+		} else {
+			ret := map[string]any{
+				"expire": token.Expire.Unix(),
+				"token":  token.Token.Raw,
+			}
+			return nil, errorx.NewCode(0, "登录成功", ret)
+		}
+	}
+}
+func (c *authController) Register(ctx context.Context, req *api.AuthRegisterReq) (res *api.AuthRegisterRes, err error) {
+	var (
+		r        = g.RequestFromCtx(ctx)
+		password []byte
+	)
+
+	user := &model.User{
+		Name:  req.Username,
+		Email: req.Username,
+		IP:    r.GetClientIp(),
+	}
+	if password, err = bcrypt.GenerateFromPassword(consts.Salt(req.Password), bcrypt.DefaultCost); err != nil {
+		return nil, err
+	} else {
+		user.Password = string(password)
+	}
+	send := &api.AuthSendReq{
+		Mail: req.Username,
+		Type: "register",
+	}
+	if err = c.verifyCode(ctx, send, req.Code); err != nil {
+		return nil, err
+	}
+	if err = dao.User.Create(user); err != nil {
+		if strings.Contains(err.Error(), "duplicate key") {
+			return nil, errorx.New("邮箱已在系统中存在")
+		}
+		return nil, err
+	} else {
+		return nil, errorx.NewCode(0, "注册成功", nil)
+	}
+}
+func (c *authController) Send(ctx context.Context, req *api.AuthSendReq) (res *api.AuthSendRes, err error) {
+	if !lo.Contains([]string{"register", "forget"}, req.Type) {
+		return nil, errorx.New("不支持的验证码类型")
+	}
+	key := c.sendKey(req.Mail, req.Type)
+	var code = gconv.String(grand.N(100000, 999999))
+	var expire = time.Duration(mail.Conf.Expire) * time.Second
+	var balance time.Duration
+	if balance, err = cache.GetExpire(ctx, key); err == nil {
+		ex := int64(expire.Seconds()) - int64(balance.Seconds())
+		if ex < mail.Conf.DiffTime {
+			return nil, errorx.New("验证码已发送，请过一会儿再请求")
+		}
+	}
+	params := map[string]any{
+		"name":   consts.AppName,
+		"code":   code,
+		"expire": int64(expire.Seconds()) / 60,
+		"url":    "https://baidu.com",
+	}
+	u := dao.User
+	if _, err = u.Where(u.Email.Like(req.Mail)).First(); err != nil {
+		if req.Type == "forget" {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return nil, errorx.New("邮箱未注册")
+			}
+			return nil, err
+		}
+	} else {
+		if req.Type == "register" {
+			return nil, errorx.New("邮箱已在系统中存在")
+		}
+	}
+	if err = mail.Send(ctx, req.Mail, mail.RegisterTPL, params); err != nil {
+		return nil, err
+	}
+	if err = cache.Set(ctx, c.sendKey(req.Mail, req.Type), code, expire); err != nil {
+		return nil, errorx.New("缓存验证码失败")
+	}
+	errKey := c.sendKey(req.Mail, req.Type+"|error")
+	_, _ = cache.Remove(ctx, errKey)
+	return nil, errorx.NewCode(0, "发送成功", code)
+}
+func (c *authController) verifyCode(ctx context.Context, req *api.AuthSendReq, code string) error {
+	var expire = time.Duration(mail.Conf.Expire) * time.Second
+	if get, err := cache.Get(ctx, c.sendKey(req.Mail, req.Type)); err != nil {
+		return errorx.New("请先获取邮箱验证码")
+	} else {
+		if !gconv.Bool(get) {
+			return errorx.New("请先获取邮箱验证码")
+		}
+		errKey := c.sendKey(req.Mail, req.Type+"|error")
+		var fail *gvar.Var
+		if fail, err = cache.Get(ctx, errKey); err == nil {
+			if fail.Int64() >= mail.Conf.ErrorTimes {
+				return errorx.New(fmt.Sprintf("错误次数过多，请重新获取"))
+			}
+		}
+		if get.String() != code {
+			var times = fail.Int64()
+			if err = cache.Set(ctx, errKey, times+1, expire); err != nil {
+				return errorx.New("更新验证码错误次数失败" + err.Error())
+			}
+			return errorx.New("验证码错误")
+		}
+		return nil
+	}
+
+}
+func (c *authController) sendKey(email string, typ string) string {
+	var buffer bytes.Buffer
+	buffer.WriteString("send|")
+	buffer.WriteString(typ)
+	buffer.WriteString("|")
+	buffer.WriteString(email)
+	return buffer.String()
+}
+func (c *authController) Forget(ctx context.Context, req *api.AuthForgetReq) (res *api.AuthForgetRes, err error) {
+	var password []byte
+	send := &api.AuthSendReq{
+		Mail: req.Username,
+		Type: "forget",
+	}
+	if err = c.verifyCode(ctx, send, req.Code); err != nil {
+		return nil, err
+	}
+	u := dao.User
+	if password, err = bcrypt.GenerateFromPassword(consts.Salt(req.Password), bcrypt.DefaultCost); err != nil {
+		return nil, err
+	} else if _, err = u.Where(u.Email.Like(req.Username)).UpdateSimple(u.Password.Value(string(password))); err != nil {
+		return nil, err
+	}
+	return nil, errorx.NewCode(0, "重置成功", nil)
+}
+func (c *authedController) Session(ctx context.Context, req *api.AuthSessionReq) (res *api.AuthSessionRes, err error) {
+	return
+}
+func (c *authedController) User(ctx context.Context, req *api.AuthUserReq) (res *api.AuthUserRes, err error) {
+	var (
+		u    = dao.User
+		r    = g.RequestFromCtx(ctx)
+		id   = r.GetParam(consts.UserKey).Int64()
+		user *model.User
+	)
+	if user, err = u.Where(u.ID.Eq(id)).First(); err != nil {
+		return nil, err
+	}
+	data := api.AuthUserRes(user)
+	res = &data
+	return
+}
+func (c *authedController) Logout(ctx context.Context, req *api.AuthLogoutReq) (res *api.AuthLogoutRes, err error) {
+	return nil, service.Auth.LogoutHandler(ctx)
+}
+func (c *authedController) Refresh(ctx context.Context, req *api.AuthRefreshReq) (res *api.AuthRefreshRes, err error) {
+	return nil, service.Auth.RefreshHandler(ctx)
+}
+func (c *authedController) Reset(ctx context.Context, req *api.AuthResetReq) (res *api.AuthResetRes, err error) {
+	var (
+		u        = dao.User
+		r        = g.RequestFromCtx(ctx)
+		id       = r.GetParam(consts.UserKey).Int64()
+		password []byte
+	)
+	if password, err = bcrypt.GenerateFromPassword(consts.Salt(req.Password), bcrypt.DefaultCost); err != nil {
+		return nil, err
+	} else if _, err = u.Where(u.ID.Eq(id)).UpdateSimple(u.Password.Value(string(password))); err != nil {
+		return nil, err
+	}
+	return nil, errorx.NewCode(0, "修改成功", nil)
+}
---
+++ go.mod
@@ -0,0 +1,83 @@
+module conset
+
+go 1.19
+
+require (
+	github.com/gogf/gf/v2 v2.2.4
+	github.com/hinego/authentic v0.0.0-20221110072710-a69ad618ba31
+	github.com/hinego/decimal v1.3.5
+	github.com/hinego/errorx v0.0.0-20221105103501-9a9e8179272b
+	github.com/hinego/gen v0.0.0-20221117091600-4eeecfa6e812
+	github.com/hinego/starter v0.0.0-20221117101308-f2fe4dc70d95
+	github.com/hinego/types v0.0.0-20221117100212-2173e3c23c2d
+	github.com/samber/lo v1.35.0
+	golang.org/x/crypto v0.3.0
+	gorm.io/gorm v1.24.1
+)
+
+require (
+	github.com/BurntSushi/toml v1.1.0 // indirect
+	github.com/alibabacloud-go/alibabacloud-gateway-spi v0.0.4 // indirect
+	github.com/alibabacloud-go/darabonba-openapi v0.2.1 // indirect
+	github.com/alibabacloud-go/debug v0.0.0-20190504072949-9472017b5c68 // indirect
+	github.com/alibabacloud-go/dm-20151123 v1.0.4 // indirect
+	github.com/alibabacloud-go/endpoint-util v1.1.0 // indirect
+	github.com/alibabacloud-go/openapi-util v0.0.11 // indirect
+	github.com/alibabacloud-go/tea v1.1.20 // indirect
+	github.com/alibabacloud-go/tea-utils v1.4.5 // indirect
+	github.com/alibabacloud-go/tea-xml v1.1.2 // indirect
+	github.com/aliyun/credentials-go v1.1.2 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/clbanning/mxj/v2 v2.5.6 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/grokify/html-strip-tags-go v0.0.1 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.13.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.12.0 // indirect
+	github.com/jackc/pgx/v4 v4.17.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/mattn/go-sqlite3 v1.14.15 // indirect
+	github.com/microsoft/go-mssqldb v0.17.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/tjfoc/gmsm v1.3.2 // indirect
+	go.opentelemetry.io/otel v1.7.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.7.0 // indirect
+	go.opentelemetry.io/otel/trace v1.7.0 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/tools v0.3.0 // indirect
+	gopkg.in/ini.v1 v1.56.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/datatypes v1.0.7 // indirect
+	gorm.io/driver/mysql v1.4.4 // indirect
+	gorm.io/driver/postgres v1.4.5 // indirect
+	gorm.io/driver/sqlite v1.4.3 // indirect
+	gorm.io/driver/sqlserver v1.4.1 // indirect
+	gorm.io/hints v1.1.1 // indirect
+	gorm.io/plugin/dbresolver v1.3.0 // indirect
+)
