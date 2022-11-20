029958a5202bb53ca09c0dffb679bf28e3abca42
hinego <89022212+hinego@users.noreply.github.com>
2022-11-20 17:02:21 +0800

    初始化数据库时支持自动生成结构

--- base/setting.go
+++ base/setting.go
@@ -5,20 +5,29 @@ import (
 	"github.com/gogf/gf/v2/os/gfile"
 )
 
+type ProLayout struct {
+	NavTheme     string `json:"navTheme"`
+	ColorPrimary string `json:"colorPrimary"`
+	Logo         string `json:"logo"`
+	Title        string `json:"title"`
+}
 type Setting struct {
-	Name     string `json:"name" dc:"标题"`
-	Logo     string `json:"logo" dc:"图标"`
-	Color    string `json:"color" dc:"主色"`
-	Theme    string `json:"theme" dc:"主题"`
-	Register bool   `json:"register" dc:"开放注册"`
-	Forget   bool   `json:"forget" dc:"开放找回"`
+	Layout   ProLayout `json:"layout"`
+	Register bool      `json:"register" dc:"开放注册"`
+	Forget   bool      `json:"forget" dc:"开放找回"`
+	Verify   bool      `json:"verify" dc:"注册验证"`
 }
 
 var DefaultSetting = Setting{
-	Name:  "默认",
-	Logo:  "/logo.svg",
-	Color: "#13C2C2",
-	Theme: "light",
+	Layout: ProLayout{
+		NavTheme:     "light",
+		ColorPrimary: "#13C2C2",
+		Logo:         "/logo.svg",
+		Title:        "默认名称",
+	},
+	Register: false,
+	Forget:   false,
+	Verify:   false,
 }
 
 const SettingPath = "setting.json"
