2a76a277fbcc72f4014b6ee75ad0271521f1c92e
hinego <89022212+hinego@users.noreply.github.com>
2022-11-20 17:52:01 +0800

    创建用户时自动创建用户名称

--- base/setting.go
+++ base/setting.go
@@ -17,6 +17,7 @@ type Setting struct {
 	Register bool      `json:"register" dc:"开放注册"`
 	Forget   bool      `json:"forget" dc:"开放找回"`
 	Verify   bool      `json:"verify" dc:"注册验证"`
+	Desc     string    `json:"desc" dc:"登录说明"`
 }
 
 var DefaultSetting = Setting{
@@ -29,6 +30,7 @@ var DefaultSetting = Setting{
 	Register: false,
 	Forget:   false,
 	Verify:   false,
+	Desc:     "最牛逼的卖牛平台面板",
 }
 
 const SettingPath = "setting.json"
