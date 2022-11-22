83b559e38e324a21609ec58aa1d2a2325c9f8a8b
hinego <89022212+hinego@users.noreply.github.com>
2022-11-22 19:53:05 +0800

    更新 修改包名

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"github.com/gogf/gf/v2/os/gctx"
-	"github.com/sucold/starter/app/cmd"
 	_ "github.com/sucold/starter/app/packed"
-	"log"
 )
 
 func main() {
 	//test
-	log.SetFlags(log.Llongfile)
-	cmd.Main.Run(gctx.New())
+	//log.SetFlags(log.Llongfile)
+	//cmd.Main.Run(gctx.New())
 }
